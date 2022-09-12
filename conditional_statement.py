bfa31206d374da1676bff8926f3066edc72b4277
Aastha Dahal <aasthadahal183@gmail.com>
2022-09-12 10:19:28 +0545

    Conditional statement

---
+++ conditional_statement.py
@@ -0,0 +1,10 @@
+# import flask
+age = int(input("Enter your age"))
+if 0<age<= 10:    #age is greater than 0 and less than 10
+    print(f"You are {age} years old which is less than 10 and greater than 0 so your movie ticket is Rs 100")
+elif 10 <age <=20:
+    print(f"You are {age} years old which is greater than 10 and less than 20 so your movie ticket is Rs 200")
+elif 20<age<59:
+    print(f"You are {age} years old which is greater than 20 and less than 59 so your movie ticket is Rs 200")
+else:
+    print(f"You are {age} years old so your movie ticket is free")
